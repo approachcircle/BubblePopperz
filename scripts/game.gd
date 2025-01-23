ef04c1c8de4a00cd0a391fbb38d7e9e8f88ca5a6
Oscar McDermott <58401347+approachcircle@users.noreply.github.com>
2025-01-23 14:47:18 +0000

    spawn shield bubbles, add power up display

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 var default_spawn_time = 1.5
 var spawn_time = default_spawn_time
-var diff_cap = 0.35
+# var spawn_time = 0.35
+var diff_cap = 0.5
 
 func _ready() -> void:
 	if Globals.current_game_mode != Globals.GameMode.Limitless:
 		var health_display: PackedScene = preload("res://scenes/HealthDisplay.tscn")
 		self.add_child(health_display.instantiate())
+	if Globals.current_game_mode != Globals.GameMode.Powerless:
+		pass
+		# var power_up_display: PackedScene = preload("res://scenes/PowerUpDisplay.tscn")
+		# self.add_child(power_up_display.instantiate())
+		# TODO: auto add power up display by the end, display will always be visible
+		# for now
 	start_spawn_bubbles()
 	
 func start_spawn_bubbles():
@@ -18,14 +25,16 @@ func start_spawn_bubbles():
 		var readable_diff = str(snapped((default_spawn_time - spawn_time) * 1000, 0.1))
 		var readable_diff_cap = str(snapped((default_spawn_time - diff_cap) * 1000, 0.1))
 		difficulty.text = "Difficulty: " + readable_diff
-		if readable_diff == readable_diff_cap:
+		if int(readable_diff) >= int(readable_diff_cap):
 			difficulty.text = difficulty.text + " (MAX)"
+		roll_power_up()
 		var bubble: PackedScene = preload("res://scenes/ExpandingBubble.tscn")
 		var bubble_node: Node2D = bubble.instantiate()
 		bubble_node.bubble_missed.connect(func lambda(): bubble_missed(bubble_node))
 		var vec = Vector2()
 		var scr_size = DisplayServer.window_get_size()
-		vec.x = randi_range(0, scr_size.x - 400)
+		var sidebar: ColorRect = get_node("Sidebar")
+		vec.x = randi_range(0, scr_size.x - (sidebar.size.x + 200))
 		vec.y = randi_range(0, scr_size.y - 400)
 		bubble_node.position = vec
 		self.add_child(bubble_node)
@@ -35,7 +44,19 @@ func start_spawn_bubbles():
 		await timer.timeout
 		if spawn_time > diff_cap:
 			spawn_time -= 0.03
-		
+
+func roll_power_up():
+	for i in Globals.PowerUp.size():
+		var upper = 10
+		var roll = randi_range(1, upper)
+		if roll == upper / 2:
+			Globals.current_power_up = i
+			print("power up selected as: " + str(Globals.PowerUp.find_key(i)))
+			print(Globals.PowerUp.find_key(i))
+			break
+		else:
+			Globals.current_power_up = Globals.PowerUp.Normal
+
 func bubble_missed(bubble: Node2D):
 	var missed: PackedScene = preload("res://scenes/BubbleMissed.tscn")
 	var missed_node: Node2D = missed.instantiate()
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -9,15 +9,20 @@ enum GameMode {
 enum PowerUp {
 	Freeze,
 	Nuke,
-	Shield
+	Shield,
+	Normal
 }
 
 var current_game_mode = GameMode.Normal
+var current_power_up = PowerUp.Normal
+var powerups = []
 var health = 1.0
 
 func load_defaults():
 	health = 1.0
 	current_game_mode = GameMode.Normal
+	current_power_up = PowerUp.Normal
+	powerups = []
 
 func _ready() -> void:
 	pass
