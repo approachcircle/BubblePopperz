0eb02ea18ac0b88609b9f00e3b44257106ded9ee
Oscar McDermott <58401347+approachcircle@users.noreply.github.com>
2025-01-28 15:16:18 +0000

    work on tutorial

--- scripts/lesson_1.gd
+++ scripts/lesson_1.gd
@@ -4,8 +4,8 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var bubble = preload("res://scenes/ExpandingBubble.tscn").instantiate()
-	bubble.x = DisplayServer.window_get_size().x / 2
-	bubble.y = DisplayServer.window_get_size().y / 2
+	bubble.position.x = DisplayServer.window_get_size().x / 2
+	bubble.position.y = DisplayServer.window_get_size().y / 2
 	self.add_child(bubble)
 
 
