3bd1d6b35b32a2ed30d30093b050f0ee02f8b7e4
Oscar McDermott <58401347+approachcircle@users.noreply.github.com>
2025-01-28 15:16:18 +0000

    work on tutorial

--- scripts/lesson_nav.gd
+++ scripts/lesson_nav.gd
@@ -9,10 +9,18 @@ func _ready() -> void:
 				get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
 			else:
 				Globals.current_lesson += 1
+				# if the transfer fails, revert our lesson number back
+				if transfer_to_new_lesson() == null:
+					Globals.current_lesson -= 1
 	)
 	get_node("Back").pressed.connect(
 		func lambda():
 			Globals.current_lesson -= 1
+			if Globals.current_lesson == 0:
+				get_tree().change_scene_to_packed(load("res://scenes/Lessons/Intro.tscn"))
+			else:
+				if transfer_to_new_lesson() == null:
+					Globals.current_lesson += 1
 	)
 
 
@@ -23,3 +31,8 @@ func _process(delta: float) -> void:
 		get_node("Next").text = "End"
 	else:
 		get_node("Next").text = "Next"
+
+func transfer_to_new_lesson() -> Resource:
+	var new_lesson = load("res://scenes/Lessons/Lesson" + str(Globals.current_lesson) + ".tscn")
+	get_tree().change_scene_to_packed(new_lesson)
+	return new_lesson
